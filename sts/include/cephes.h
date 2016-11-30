71dd0148c5ed603bcfae0b31fde7aea4cea06ecf
tpu01yzx <tpu01yzx@163.com>
2016-11-30 19:05:48 +0800

    Make choice parameterized.

--- sts/include/cephes.h
+++ sts/include/cephes.h
@@ -11,4 +11,9 @@ double cephes_erf(double x);
 double cephes_erfc(double x);
 double cephes_normal(double x);
 
+#ifdef _MSC_VER
+double erf(double x);
+double erfc(double x);
+#endif
+
 #endif /*  _CEPHES_H_  */
