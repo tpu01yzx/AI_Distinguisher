21dcd58bb822f8498091504b068ddc42b5a183b5
NULL Ying <ourweijiang@gmail.com>
2016-12-04 20:02:02 +0800

    aes add

--- cipher/aes/main.c
+++ cipher/aes/main.c
@@ -1,7 +1,12 @@
 #include <stdio.h>
 #include <string.h>
-#include <stdint.h>
 #include <ctype.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <getopt.h>
+#include <math.h>
+#include <time.h>
 
 // Enable both ECB and CBC mode. Note this can be done before including aes.h or at compile-time.
 // E.g. with GCC by using the -D flag: gcc -c aes.c -DCBC=0 -DECB=1
@@ -10,23 +15,102 @@
 
 #include "aes.h"
 
-static void phex(uint8_t* str);
-static void test_encrypt_ecb_verbose(void);
-static void test_Decrypt_ecb_verbose(void);
+void phex(uint8_t* str);
+void encrypt_ecb_verbose(uint8_t *plain_text,int len);
 
+int len;
+unsigned char *key=NULL;
+char *output=NULL;
+int output_key = 0;
 
-int main(void)
-{
-	test_encrypt_ecb_verbose();
-	test_Decrypt_ecb_verbose();
+int generate(){
+	clock_t start, end;
+	start = clock();
+	//set start time
+	srand(time(0));
+	//set srand
+	uint8_t *buf = malloc(len);
+	if(buf == NULL){
+		perror("malloc");
+		return -1;
+	}
+	//malloc buf
+	int i;
+	for(i=0;i<len;i++){
+		buf[i] = rand()%256;
+	}
+    encrypt_ecb_verbose(buf,len);
+	//
+	FILE *fp;
+	fp = fopen(output, "w");
+	if(fp == NULL){
+		perror("fopen");
+		return -1;
+	}
+	fwrite(buf, len, 1, fp);
+	fclose(fp);
+	//saveto file
+	end = clock();
+	double diff = (double)(end-start)/CLOCKS_PER_SEC;
+	if(output_key){
+		fwrite(buf, len, 1, stdout);
+	}else{
+		fprintf(stdout, "Generate %d bytes (%.2fMB) in %f s, %.2f MB/s\n", len, (double)len/1024/1024, diff, len/1024/1024/diff);
+	}
+	return 0;
+}
+
+void usage(const char *progname){
+	fprintf(stderr, "AES generator\n"
+  "Usage: %s\n"
+			"\t-l lenght(byte)\n"
+			"\t-k key\n"
+			"\t-o output cipher stream to file\n"
+			"\t-q output key stream to stdout\n"
+			"\n",
+		progname);
+}
+
+int main(int argc, char *argv[]){
+	int ch;
+
+	if(argc == 1){
+		usage(argv[0]);
+		return -1;
+	}
+
+	while((ch = getopt(argc, argv, "l:k:o:q")) != -1){
+		switch(ch){
+			case 'l':
+				len = atoi(optarg);
+				break;
+			case 'k':
+				key = (unsigned char *)optarg;
+				break;
+			case 'o':
+				output = optarg;
+				break;
+			case 'q':
+				output_key = 1;
+				break;
+			default:
+				usage(argv[0]);
+				return -1;
+		}
+	}
+
+	if(output == NULL || key == NULL || len == 0){
+		usage(argv[0]);
+		return -1;
+	}
+
+	generate();
 
 	return 0;
 }
 
-
-
 // prints string as hex
-static void phex(uint8_t* str)
+void phex(uint8_t* str)
 {
 	unsigned char i;
 	for (i = 0; i < 16; ++i)
@@ -34,101 +118,37 @@ static void phex(uint8_t* str)
 	printf("\n");
 }
 
-void HexStrToByte(const char* source, unsigned char* dest, int sourceLen)
-{
-	short i;
-	unsigned char highByte, lowByte;
-
-	for (i = 0; i < sourceLen; i += 2)
-	{
-		highByte = toupper(source[i]);
-		lowByte = toupper(source[i + 1]);
-
-		if (highByte > 0x39)
-			highByte -= 0x37;
-		else
-			highByte -= 0x30;
-
-		if (lowByte > 0x39)
-			lowByte -= 0x37;
-		else
-			lowByte -= 0x30;
-
-		dest[i / 2] = (highByte << 4) | lowByte;
-	}
-	return;
-}
-
-static void test_encrypt_ecb_verbose(void)
+// 128bit key
+void encrypt_ecb_verbose(uint8_t *plain_text,int len)
 {
 	// Example of more verbose verification
 
-	uint8_t i, buf[320];
+	uint8_t i;
+	uint8_t *buf = malloc(len);
+	memset(buf, 0, len);
 
-	// 128bit key
-	uint8_t key[16] = { (uint8_t)0x5A,  (uint8_t)0x3C,  (uint8_t)0x6F,  (uint8_t)0x5C,  (uint8_t)0x53,  (uint8_t)0x49,  (uint8_t)0x76,  (uint8_t)0x70,  (uint8_t)0x69,  (uint8_t)0x3A,  (uint8_t)0x73,  (uint8_t)0x66,  (uint8_t)0x56,  (uint8_t)0x45, (uint8_t)0x22, (uint8_t)0x28};
-	// 512bit text
-	uint8_t *plain_text = (uint8_t *)" ";
-
-	memset(buf, 0, 320);
-
-	// print text to encrypt, key and IV
-	printf("ECB encrypt verbose:\n\n");
-	printf("plain text:\n");
-	for (i = (uint8_t)0; i < (uint8_t)4; ++i)
-	{
-		phex(plain_text + i * (uint8_t)16);
+	if(!output_key){
+	    // print text to encrypt, key and IV
+	    printf("ECB encrypt verbose:\n\n");
+	    printf("plain text:\n");
+	    for (i = (uint8_t)0; i < (uint8_t)4; ++i)
+	    {
+	    	phex(plain_text + i * (uint8_t)16);
+	    }
+	    printf("\n");
+    
+	    printf("key:");
+	    phex(key);
+	    printf("\n");
+		// print the resulting cipher as 4 x 16 byte strings
+	    printf("ciphertext:\n");
 	}
-	printf("\n");
-
-	printf("key:\n");
-	phex(key);
-	printf("\n");
-
-	// print the resulting cipher as 4 x 16 byte strings
-	printf("ciphertext:\n");
-	for (i = 0; i < 19; ++i)
+	for (i = 0; i < (len/16); ++i)
 	{
 		AES128_ECB_encrypt(plain_text + (i * 16), key, buf + (i * 16));
-		phex(buf + (i * 16));
+		memcpy(plain_text + (i * 16),(buf + (i * 16)),16);
 	}
-	printf("\n");
+	if(!output_key)
+		phex(plain_text);
 }
-
-static void test_Decrypt_ecb_verbose(void)
-{
-	// Example of more verbose verification
-
-	uint8_t i, buf[640], buf2[640];
-
-	// 128bit key
-	uint8_t key[16] = { (uint8_t)0x5A,  (uint8_t)0x3C,  (uint8_t)0x6F,  (uint8_t)0x5C,  (uint8_t)0x53,  (uint8_t)0x49,  (uint8_t)0x76,  (uint8_t)0x70,  (uint8_t)0x69,  (uint8_t)0x3A,  (uint8_t)0x73,  (uint8_t)0x66,  (uint8_t)0x56,  (uint8_t)0x45, (uint8_t)0x22, (uint8_t)0x28 };
-	// 512bit text
-	uint8_t *plain_text = (uint8_t *)" ";
-
-	memset(buf, 0, 640);
-	memset(buf2, 0, 640);
-
-	// print text to encrypt, key and IV
-	printf("ECB encrypt verbose:\n\n");
-	printf("plain text:\n");
-	printf((char *)plain_text);
-	printf("\n");
-
-	printf("key:\n");
-	phex(key);
-	printf("\n");
-
-	// print the resulting cipher as 4 x 16 byte strings
-	printf("ciphertext:\n");
-	HexStrToByte((char *)plain_text, buf2, 640-1);
-	for (i = 0; i < 19; ++i)
-	{
-		AES128_ECB_decrypt(buf2 + (i * 16), key, buf + (i * 16));
-		printf("%s",buf + (i * 16));
-	}
-	printf("\n");
-}
-
-
 
