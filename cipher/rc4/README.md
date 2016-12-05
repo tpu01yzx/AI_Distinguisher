824059919c875a19c8044f7b98e781a6ddeeaddf
Tuber <CEC0D5F1B6AB@qq.com>
2016-12-05 11:14:19 +0800

    优化文件交互方式，提高通用性 增加命令行参数 数据分段写入

--- cipher/rc4/README.md
+++ cipher/rc4/README.md
@@ -1,14 +1,16 @@
-Usage: ./rc4 -l lenght(byte) -k key -o output
+Usage: ./rc4 -l lenght(byte)
 
-  -l 生成的长度
+  -l 需要生成的加密流长度
+  
+  -b 数据分块的大小，默认为8KB
 
-  -k 密钥
+  -k 密钥，默认为000000（ASCII）
 
-  -o 输出文件名
+  -o 输出加密流文件，默认为/dev/stdout 
 
-  -q 输出密钥流到stdout
-
-PS：明文采取rand函数根据时间生成
+  -s 输出密钥流到文件，默认为/dev/null（即不输出）
+  
+  -i 输入的数据流文件，默认为/dev/urandom （unix随机流）
 
 
 Example：
@@ -19,4 +21,14 @@ Example：
 
   输出到 当前目录 o.dat
 
-  rc4 -l 104857600 -k 123456 -o o.dat
+  ./rc4 -l 104857600 -k 123456 -o o.dat
+  
+PS:
+    标准输出流 /dev/stdout
+    标准输入流 /dev/stdin
+    标准错误流 /dev/stderr
+    空设备（黑洞）/dev/null
+    全是0的文件 /dev/zero
+    “真”随机 /dev/random 会产生阻塞，效果好
+    “伪”随机 /dev/urandom 不会阻塞，效果没那么好
+    网络流 /dev/tcp[udp]/host/port 前提是建立了连接
--- cipher/rc4/main.c
+++ cipher/rc4/main.c
@@ -3,102 +3,113 @@
 #include <unistd.h>
 #include <string.h>
 #include <getopt.h>
-#include <math.h>
-#include <time.h>
 
 #include "rc4.c"
 
-int len;
-unsigned char *key=NULL;
-char *output=NULL;
-int output_key = 0;
+int block_size = 8192;//default block size
+long long int output_len = 0;
+unsigned char *key = "000000";//default key 000000
+char *input_dats = "/dev/urandom";
+char *output_dats = "/dev/stdout";
+char *output_keys = "/dev/null";
 
 int generate(){
 	clock_t start, end;
 	start = clock();
 	//set start time
-	srand(time(0));
-	//set srand
-	char *buf = malloc(len);
-	char *mbuf = malloc(len);
-	if(buf == NULL){
+	FILE *dats_src, *fp_dats, *fp_keys;
+	dats_src = fopen(input_dats, "rb");
+    fp_dats = fopen(output_dats, "wb");
+    fp_keys = fopen(output_keys, "wb");
+	if(!dats_src || !fp_dats || !fp_keys){
+		perror("fopen");
+		return -1;
+	}
+	//open file
+	char *buf = malloc(block_size);
+	char *mbuf = malloc(block_size);
+	if(!buf || !mbuf){
 		perror("malloc");
 		return -1;
 	}
 	//malloc buf
-	int i;
-	for(i=0;i<len;i++){
-		buf[i] = rand()%256;
+	long long int len = output_len;
+	int i, ret = 1;
+	while(len > 0 && ret){
+        int l = len < block_size ? len : block_size;
+        ret = fread(buf, 1, l, dats_src);
+        len -= ret;
+        memcpy(mbuf, buf, l);
+        //fill buf
+        rc4_crypt(mbuf, l, key, strlen(key));
+        //enc
+        for(i=0;i<l;i++){
+            buf[i] = buf[i] ^ mbuf[i];
+        }
+        //get key stream
+        fwrite(buf, ret, 1, fp_dats);
+        fwrite(mbuf, ret, 1, fp_keys);
 	}
-	memcpy(mbuf, buf, len);
-	//fill buf
-	rc4_crypt(mbuf, len, key, strlen(key));
-	//enc
-	for(i=0;i<len;i++){
-		buf[i] = buf[i] ^ mbuf[i];
-	}
-	//
-	FILE *fp;
-	fp = fopen(output, "w");
-	if(fp == NULL){
-		perror("fopen");
-		return -1;
-	}
-	fwrite(buf, len, 1, fp);
-	fclose(fp);
-	//saveto file
-	end = clock();
-	double diff = (double)(end-start)/CLOCKS_PER_SEC;
-	if(output_key){
-		fwrite(buf, len, 1, stdout);
-	}else{
-		fprintf(stdout, "Generate %d bytes (%.2fMB) in %f s, %.2f MB/s\n", len, (double)len/1024/1024, diff, len/1024/1024/diff);
-	}
+
+    free(buf);
+    free(mbuf);
+
+    fclose(dats_src);
+	fclose(fp_dats);
+	fclose(fp_keys);
+	//close file
 	return 0;
 }
 
 void usage(const char *progname){
-	fprintf(stderr, "Rc4 generator\n"
-  "Usage: %s\n"
-			"\t-l lenght(byte)\n"
-			"\t-k key\n"
-			"\t-o output cipher stream to file\n"
-			"\t-q output key stream to stdout\n"
+	fprintf(stderr, "RC4 stream generator\n"
+  "Usage: %s [options] -l length\n"
+			"\t-l   Output lenght (byte)\n"
+			"\t-b   Block size (byte), default is 8192\n"
+			"\t-k   Encryption key, default is 000000 (ASCII)\n"
+			"\t-o   Cipher stream to file, default is /dev/stdout\n"
+			"\t-s   Secret key stream to file, default is /dev/null\n"
+			"\t-i   Encrypted stream from file, default is /dev/urandom\n"
+			"\t-h   Show this message\n"
 			"\n",
 		progname);
 }
 
 int main(int argc, char *argv[]){
-	int ch;
-
-	if(argc == 1){
-		usage(argv[0]);
-		return -1;
-	}
-
-	while((ch = getopt(argc, argv, "l:k:o:q")) != -1){
+    int ch;
+	while((ch = getopt(argc, argv, "l:b:k:o:s:i:h")) != -1){
 		switch(ch){
 			case 'l':
-				len = atoi(optarg);
+				output_len = atoll(optarg);
+				break;
+            case 'b':
+				block_size = atoi(optarg);
 				break;
 			case 'k':
 				key = optarg;
 				break;
-			case 'o':
-				output = optarg;
+            case 'i':
+				input_dats = optarg;
 				break;
-			case 'q':
-				output_key = 1;
+			case 'o':
+				output_dats = optarg;
+				break;
+			case 's':
+				output_keys = optarg;
+				break;
+            case 'h':
+				usage(argv[0]);
+				return 0;
 				break;
 			default:
 				usage(argv[0]);
 				return -1;
 		}
 	}
-
-	if(output == NULL || key == NULL || len == 0){
-		usage(argv[0]);
-		return -1;
+	
+	if(!output_len){
+        usage(argv[0]);
+        return -1;
 	}
 
 	generate();
